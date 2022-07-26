5e58c5de7822bc253182298bf6ff3e99fcff3a5f
Tanzim Ahmed <109636151+tanzim2000@users.noreply.github.com>
2022-07-26 13:26:27 -0400

    Add files via upload

---
+++ Assignment 02/resources/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="Author" content="Tanzim Ahmed Sagar">
+            <meta name="Group" content="04">
+            <meta name="Description" content="Assignment 02">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link href="https://fonts.googleapis.com/css2?family=Saira:wght@800&Caveat:wght@500&family=Great+Vibes&family=Lobster+Two:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="css/uniform.css">
+            <link rel="stylesheet" href="css/homepage.css">
+            <title>Children Immunization Record System (CIRS)</title>
+        </head>
---
+++ Assignment 02/resources/navbar.php
@@ -0,0 +1,10 @@
+        <body>
+            <!-- ============= Sticky NavBar ============= -->
+            <nav class="header navbar">
+                <a href="index.html" id="logo"><h3>C I R S</h3></a>
+                <ul>
+                    <li><a class="navanchor" href="">Vaccines</a></li>
+                    <li><a class="navanchor" href="">Register/Login</a></li>
+                    <li><a class="navanchor" href="">About Us</a></li>
+                </ul>
+            </nav>
